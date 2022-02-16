24688598de75d20c0fb631b6c6b39c7b6a4529e0
Joaquin Correa <jcorrea@focus.uy>
2022-02-15 21:24:48 -0300

    Sacar contador

--- lopy-lopy/lopy-A/main.py
+++ lopy-lopy/lopy-A/main.py
@@ -17,10 +17,7 @@ s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 print('Start')
 
-i=0
 while True:
     if s.recv(64) == b'Ping':
         print('Ping Recived')
-        s.send('Pong'+ i)
-        i+1
     time.sleep(5)
